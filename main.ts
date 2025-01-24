34268131e1421dcfcfeed7df6c42351a24d63319
PoloCotarelo <polo.cotarelo@gmail.com>
2025-01-24 12:20:08 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
+let espera = 0
 let fila = 0
 let columna = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(randint(101, 999))
+    basic.showNumber(randint(11, 99))
 })
+function paso1 () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P3, 0)
+    basic.pause(espera)
+}
+function paso2 () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P3, 0)
+    basic.pause(espera)
+}
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
         # # # # #
@@ -11,12 +26,14 @@ input.onButtonPressed(Button.B, function () {
         # # # # #
         # # # # #
         `)
+    espera = 50
     fila = 0
     columna = 0
     for (let index = 0; index < 25; index++) {
-        kitronik_motor_driver.stepperMotorTurnAngle(kitronik_motor_driver.MotorDirection.Forward, 180)
         led.toggle(fila, columna)
-        basic.pause(1000)
+        for (let index = 0; index < 10; index++) {
+        	
+        }
         fila += 1
         if (fila == 5) {
             fila = 0
